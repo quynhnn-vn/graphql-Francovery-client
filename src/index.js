b39a88d84118d9350c2404033d70172721ffe2b5
quynhnn-vn <nnquynh.0111@gmail.com>
2021-09-23 15:54:35 +0200

    Update uri in index.js for production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://francovery-server.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
