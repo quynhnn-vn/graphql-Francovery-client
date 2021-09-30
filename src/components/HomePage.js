42d60ada34c5e15bfb8abd48664400cd1c71f9dc
quynhnn-vn <nnquynh.0111@gmail.com>
2021-09-30 23:38:55 +0200

    Remove video background for speeding

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,39 +1,17 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "../styles/HomePage.scss";
 import ReactTooltip from "react-tooltip";
 import MapChart from "./MapChart";
 
 /*
-  HomePage component render a video as background,
-  a France map with tooltip displayed when hovering over each part of the map
+  HomePage component render a France map
+  with tooltip displayed when hovering over each part of the map
 */
 export default function HomePage() {
   const [content, setContent] = useState("");
-  const [loadingVideo, setLoadingVideo] = useState(true);
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef) {
-      videoRef.current.addEventListener("loadeddata", () => {
-        setLoadingVideo(false);
-      });
-      videoRef.current.removeEventListener("loadeddata", () => {
-        setLoadingVideo(true);
-      });
-    }
-  });
 
   return (
     <div className="homepage-container">
-      <video
-        ref={videoRef}
-        autoPlay
-        loop
-        muted
-        style={{ opacity: loadingVideo ? 0 : 0.7 }}
-      >
-        <source src="/pics/background.mp4" type="video/mp4" />
-      </video>
       <MapChart setTooltipContent={setContent} />
       <ReactTooltip>{content}</ReactTooltip>
     </div>
